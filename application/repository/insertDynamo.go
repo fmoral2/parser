8f7622ac6a81b74ebd92641f0ed3bccc88f1a317
Francisco Moral <francisco@rain.us>
2021-11-15 16:52:08 -0300

    DynamoDb

--- application/repository/insertDynamo.go
+++ application/repository/insertDynamo.go
@@ -8,13 +8,14 @@ import (
 	"github.com/morlfm/csv_parser/application/model"
 )
 
-func CreateTableDynamo() {
+func CreateTableDynamoDb() {
 	err := CreateTable()
 	if err != nil {
 		log.Println(err)
 	}
 }
-func PutItemDynamo(empList []model.Employee) {
+
+func PutItemDynamoDb(empList []model.Employee) {
 	validatedList := parser.ValidateEmployees(empList)
 	for _, e := range validatedList {
 		err := PutItem(e)
